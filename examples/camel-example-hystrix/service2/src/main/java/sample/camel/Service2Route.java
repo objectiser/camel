38226fd7609e3cf3ae967e7e2f97b61e2f0e998d
Gary Brown <gary@brownuk.com>
2017-04-25 16:12:13 +0100

    Create the opentracing tracer on the context

--- examples/camel-example-hystrix/service2/src/main/java/sample/camel/Service2Route.java
+++ examples/camel-example-hystrix/service2/src/main/java/sample/camel/Service2Route.java
@@ -18,14 +18,37 @@ package sample.camel;
 
 import org.apache.camel.builder.RouteBuilder;
 
+import org.apache.camel.opentracing.OpenTracingTracer;
+
+import com.uber.jaeger.Tracer;
+import com.uber.jaeger.metrics.Metrics;
+import com.uber.jaeger.metrics.NullStatsReporter;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.reporters.Reporter;
+import com.uber.jaeger.samplers.ConstSampler;
+import com.uber.jaeger.samplers.Sampler;
+import com.uber.jaeger.senders.Sender;
+import com.uber.jaeger.senders.UDPSender;
+
 public class Service2Route extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+	OpenTracingTracer ottracer = new OpenTracingTracer();
+	ottracer.setTracer(initTracer());
+	ottracer.init(getContext());
+
         from("undertow:http://0.0.0.0:7070/service2").routeId("service2").streamCaching()
                 .log(" Service2 request: ${body}")
                 .transform(simple("Service2-${body}"))
                 .log("Service2 response: ${body}");
     }
 
+    public static io.opentracing.Tracer initTracer() {
+        Sampler sampler = new ConstSampler(true);
+        Sender sender = new UDPSender(null, 0, 0);
+        Reporter reporter = new RemoteReporter(sender, 500, 1000, Metrics.fromStatsReporter(new NullStatsReporter()));
+        Tracer tracer = new Tracer.Builder("service2", reporter, sampler).build();
+        return tracer;
+    }
 }
